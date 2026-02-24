6434f7d3e9e21df9ec9d91e10a31046cbb4fc2cb
sneema-design <sneema@gammastack.com>
2026-02-24 14:57:30 +0530

    chore/added-env-variable

--- dbConnection.js
+++ dbConnection.js
@@ -1,10 +1,11 @@
 const dotenv = require("dotenv");
 const { Sequelize } = require("sequelize");
 dotenv.config();
-const sequelize = new Sequelize("demodb", "postgres", process.env.DB_PASS, {
-  host: "localhost",
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
   dialect: "postgres",
   logging: false,
+  port: process.env.DB_PORT || 5432,
 });
 
 async function connectDb(params) {
