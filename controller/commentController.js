ee6107f0082b55ebdbc9bc9fb585d4fe68bec7d5
sneema-design <sneema@gammastack.com>
2026-02-27 12:54:21 +0530

    feat-added comment apis

--- controller/commentController.js
+++ controller/commentController.js
@@ -1,32 +1,66 @@
-const { Post, User, Comment } = require("../models");
+const { Error } = require("sequelize");
+const commentService = require("../service/comment.Service");
 
-const createComments=async(req,res)=>{
+const createComments = async (req, res) => {
+  try {
+    const comment = await commentService.createComment(req.body);
+    res.status(201).json(comment);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getAllComments = async (req, res) => {
+  try {
+    const comments = await commentService.getAllComments();
+    res.status(200).json(comments);
+  } catch (error) {
+    if (error.message === "Comments not found") {
+      return res.status(404).json({ message: error.message });
+    }
+    res.status(500).json({ message: error.message });
+  }
+};
+const getCommentsById=async(req,res)=>{
     try {
-        const comment= await Comment.create(req.body);
+        const id=req.params.id;
+        if(!id){
+            throw new Error("Please provide a id");
+        }
+        const comment=await commentService.getCommentsById(id);
+        res.status(200).json(comment)
+    } catch (error) {
+        throw new Error(error)
+    }
+}
+const getCommentsByUserId=async(req,res)=>{
+    try {
+        const id=req.params.id;
+        if(!id){
+            throw new Error("Please Provide a user Id")
+        }
+        const comment = await commentService.getCommentsByUserId(id);
         res.status(200).json(comment);
     } catch (error) {
-        res.status(500).json({message:error.message})
+        throw new Error(error)
     }
 }
-
-const getAllComments=async(req,res)=>{
+const getCommentsByPostId=async(req,res)=>{
     try {
-        const comments= await Comment.findAll({include:[
-            {model:Post,as:"posts"},
-            {model:User,as:"users"}
-        ]});
-        if(comments.length==0){
-            return res.status(404).json(
-                {message:"comments not found"}
-            )
+         const id=req.params.id;
+        if(!id){
+            throw new Error("Please Provide a Post Id")
         }
-        res.status(200).json(comments);
+        const comments=await commentService.getCommentsByPostId(id);
+        res.status(200).json(comments)
     } catch (error) {
-        res.status(500).json({message:error.message})
+        throw new Error(error)
     }
 }
-
-module.exports={
-    createComments,
-    getAllComments
-}
+module.exports = {
+  createComments,
+  getAllComments,
+  getCommentsById,
+  getCommentsByUserId,
+  getCommentsByPostId
+};
--- routes/commentsRoutes.js
+++ routes/commentsRoutes.js
@@ -2,10 +2,15 @@ const express = require("express");
 const {
   createComments,
   getAllComments,
+  getCommentsById,
+  getCommentsByUserId,
+  getCommentsByPostId
 } = require("../controller/commentController");
 
 const { router } = express();
 router.post("/", createComments);
 router.get("/", getAllComments);
-
+router.get("/:id",getCommentsById);
+router.get("/user/:id",getCommentsByUserId)
+router.get("/post/:id",getCommentsByPostId)
 module.exports = router;
