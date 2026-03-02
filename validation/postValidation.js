b51df0447ee8ee469a80ae9c0cfe9b1a2fb51165
sneema-design <sneema@gammastack.com>
2026-03-02 14:42:31 +0530

    added comment schema for zod

--- validation/postValidation.js
+++ validation/postValidation.js
@@ -1,16 +1,26 @@
 const { z } = require("zod");
 
-const createPostValidation=z.object({
-    title: z.string().min(2,"Title should be minimun 2 character").max(100,"Title can be max 100 character"),
-    caption:z.string().min(2,"Caption should be minimun 2 character"),
-    userId:z.coerce.number(),
-})
-const updatePostValidation=z.object({
-    title: z.string().min(2,"Title should be minimun 2 character").max(100,"Title can be max 100 character").optional(),
-    caption:z.string().min(2,"Caption should be minimun 2 character").optional(),
-    userId:z.coerce.number().optional(),
-})
-module.exports={
-    createPostValidation,
-    updatePostValidation
-}
+const createPostValidation = z.object({
+  title: z
+    .string()
+    .min(2, "Title should be minimun 2 character")
+    .max(100, "Title can be max 100 character"),
+  caption: z.string().min(2, "Caption should be minimun 2 character"),
+  userId: z.coerce.number(),
+});
+const updatePostValidation = z.object({
+  title: z
+    .string()
+    .min(2, "Title should be minimun 2 character")
+    .max(100, "Title can be max 100 character")
+    .optional(),
+  caption: z
+    .string()
+    .min(2, "Caption should be minimun 2 character")
+    .optional(),
+  userId: z.coerce.number().optional(),
+});
+module.exports = {
+  createPostValidation,
+  updatePostValidation,
+};
