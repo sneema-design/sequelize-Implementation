97d9c32ae57bc38730eae9f00caeeb567ee9bbfc
sneema-design <sneema@gammastack.com>
2026-03-03 11:22:32 +0530

    created throw error function-Error handling

--- utils/error.js
+++ utils/error.js
@@ -6,5 +6,9 @@ const checkEmpty = (result, message) => {
   }
   return result;
 };
-
-module.exports={checkEmpty}
+const throwError=(message,statusCode)=>{
+  const error= new Error(message);
+  error.statusCode=statusCode;
+  throw error
+}
+module.exports={checkEmpty,throwError}
